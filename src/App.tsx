00f3c03c030e600577eaf337f471a79434d8b612
nafis bin <nafisbinnizam@gmail.com>
2026-04-21 02:08:03 +0600

    feat: top creator section layout done

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import TrendingCollection from './components/TrendingCollection'
+import TopCreatorSection from './components/TopCreatorSection'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Hero />
       <TrendingCollection />
+      <TopCreatorSection />
       <footer>
 
       </footer>
---
+++ src/components/TopCreatorSection.tsx
@@ -0,0 +1,313 @@
+const TopCreatorSection = () => {
+    return (
+        <div className="max-w-[1280px] h-[1085px] mx-auto px-[115px] py-[80px]">
+            {/* top creator header */}
+            <div className="flex justify-between max-w-[1050px] h-[91px]">
+                <div className="flex flex-col gap-[10px] h-full">
+                    <p className="font-work-sans text-[38px] font-semibold leading-[120%]">
+                        Top creators
+                    </p>
+                    <p className="font-work-sans text-[22px] leading-[160%]">
+                        Checkout Top Rated Creators on the NFT Marketplace
+                    </p>
+                </div>
+
+                <button className="flex gap-[12px] items-center justify-center self-baseline-last w-[247px] h-[60px] rounded-[20px] outline-2 outline-offset-[-1px] outline-ctoa">
+                    <img 
+                        className="size-[20px]"
+                        src="/icons/RocketLaunch.png" 
+                        alt="rocket icon" 
+                    />
+                    <span className="font-work-sans text-[16px] font-semibold leading-[140%]">
+                        View Rankings
+                    </span>
+                </button>
+            </div>
+
+            {/* top creator grid */}
+            <div className="grid grid-cols-4 gap-[30px] max-w-[1050px] h-[774px] mt-[60px]">
+                {/* single box */}
+                <div className="relative flex flex-col items-center gap-[20px] w-[240px] h-[238px] bg-[#3b3b3b] rounded-[20px] p-[20px]">
+                    <img 
+                        className="size-[120px] rounded-[100px]"
+                        src="/avatars/redheaded.png" 
+                        alt="" 
+                    />
+                    <div className="flex flex-col gap-[5px] w-[200px] items-center">
+                        <p className="font-work-sans font-semibold text-[22px] leading-[140%]">
+                            Keepitreal
+                        </p>
+                        <p className="font-work-sans font-normal text-[16px] leading-[140%] text-[#858584]">
+                            Total Sales: 
+                            <span className="font-space-mono font-normal text-[16px] leading-[140%] text-white">
+                                {" "}34.53 ETH
+                            </span>
+                        </p>
+                    </div>
+                    <div className="absolute top-[18px] left-[20px] size-[30px] rounded-[20px] text-[#858584] bg-[#2b2b2b] 
+                    font-space-mono text-[16px] leading-[140%] grid place-items-center">
+                        1
+                    </div>
+                </div>
+
+                <div className="relative flex flex-col items-center gap-[20px] w-[240px] h-[238px] bg-[#3b3b3b] rounded-[20px] p-[20px]">
+                    <img 
+                        className="size-[120px] rounded-[100px]"
+                        src="/avatars/redheaded.png" 
+                        alt="" 
+                    />
+                    <div className="flex flex-col gap-[5px] w-[200px] items-center">
+                        <p className="font-work-sans font-semibold text-[22px] leading-[140%]">
+                            Keepitreal
+                        </p>
+                        <p className="font-work-sans font-normal text-[16px] leading-[140%] text-[#858584]">
+                            Total Sales: 
+                            <span className="font-space-mono font-normal text-[16px] leading-[140%] text-white">
+                                {" "}34.53 ETH
+                            </span>
+                        </p>
+                    </div>
+                    <div className="absolute top-[18px] left-[20px] size-[30px] rounded-[20px] text-[#858584] bg-[#2b2b2b] 
+                    font-space-mono text-[16px] leading-[140%] grid place-items-center">
+                        1
+                    </div>
+                </div>
+
+                <div className="relative flex flex-col items-center gap-[20px] w-[240px] h-[238px] bg-[#3b3b3b] rounded-[20px] p-[20px]">
+                    <img 
+                        className="size-[120px] rounded-[100px]"
+                        src="/avatars/redheaded.png" 
+                        alt="" 
+                    />
+                    <div className="flex flex-col gap-[5px] w-[200px] items-center">
+                        <p className="font-work-sans font-semibold text-[22px] leading-[140%]">
+                            Keepitreal
+                        </p>
+                        <p className="font-work-sans font-normal text-[16px] leading-[140%] text-[#858584]">
+                            Total Sales: 
+                            <span className="font-space-mono font-normal text-[16px] leading-[140%] text-white">
+                                {" "}34.53 ETH
+                            </span>
+                        </p>
+                    </div>
+                    <div className="absolute top-[18px] left-[20px] size-[30px] rounded-[20px] text-[#858584] bg-[#2b2b2b] 
+                    font-space-mono text-[16px] leading-[140%] grid place-items-center">
+                        1
+                    </div>
+                </div>
+
+                <div className="relative flex flex-col items-center gap-[20px] w-[240px] h-[238px] bg-[#3b3b3b] rounded-[20px] p-[20px]">
+                    <img 
+                        className="size-[120px] rounded-[100px]"
+                        src="/avatars/redheaded.png" 
+                        alt="" 
+                    />
+                    <div className="flex flex-col gap-[5px] w-[200px] items-center">
+                        <p className="font-work-sans font-semibold text-[22px] leading-[140%]">
+                            Keepitreal
+                        </p>
+                        <p className="font-work-sans font-normal text-[16px] leading-[140%] text-[#858584]">
+                            Total Sales: 
+                            <span className="font-space-mono font-normal text-[16px] leading-[140%] text-white">
+                                {" "}34.53 ETH
+                            </span>
+                        </p>
+                    </div>
+                    <div className="absolute top-[18px] left-[20px] size-[30px] rounded-[20px] text-[#858584] bg-[#2b2b2b] 
+                    font-space-mono text-[16px] leading-[140%] grid place-items-center">
+                        1
+                    </div>
+                </div>
+
+                <div className="relative flex flex-col items-center gap-[20px] w-[240px] h-[238px] bg-[#3b3b3b] rounded-[20px] p-[20px]">
+                    <img 
+                        className="size-[120px] rounded-[100px]"
+                        src="/avatars/redheaded.png" 
+                        alt="" 
+                    />
+                    <div className="flex flex-col gap-[5px] w-[200px] items-center">
+                        <p className="font-work-sans font-semibold text-[22px] leading-[140%]">
+                            Keepitreal
+                        </p>
+                        <p className="font-work-sans font-normal text-[16px] leading-[140%] text-[#858584]">
+                            Total Sales: 
+                            <span className="font-space-mono font-normal text-[16px] leading-[140%] text-white">
+                                {" "}34.53 ETH
+                            </span>
+                        </p>
+                    </div>
+                    <div className="absolute top-[18px] left-[20px] size-[30px] rounded-[20px] text-[#858584] bg-[#2b2b2b] 
+                    font-space-mono text-[16px] leading-[140%] grid place-items-center">
+                        1
+                    </div>
+                </div>
+
+                <div className="relative flex flex-col items-center gap-[20px] w-[240px] h-[238px] bg-[#3b3b3b] rounded-[20px] p-[20px]">
+                    <img 
+                        className="size-[120px] rounded-[100px]"
+                        src="/avatars/redheaded.png" 
+                        alt="" 
+                    />
+                    <div className="flex flex-col gap-[5px] w-[200px] items-center">
+                        <p className="font-work-sans font-semibold text-[22px] leading-[140%]">
+                            Keepitreal
+                        </p>
+                        <p className="font-work-sans font-normal text-[16px] leading-[140%] text-[#858584]">
+                            Total Sales: 
+                            <span className="font-space-mono font-normal text-[16px] leading-[140%] text-white">
+                                {" "}34.53 ETH
+                            </span>
+                        </p>
+                    </div>
+                    <div className="absolute top-[18px] left-[20px] size-[30px] rounded-[20px] text-[#858584] bg-[#2b2b2b] 
+                    font-space-mono text-[16px] leading-[140%] grid place-items-center">
+                        1
+                    </div>
+                </div>
+
+                <div className="relative flex flex-col items-center gap-[20px] w-[240px] h-[238px] bg-[#3b3b3b] rounded-[20px] p-[20px]">
+                    <img 
+                        className="size-[120px] rounded-[100px]"
+                        src="/avatars/redheaded.png" 
+                        alt="" 
+                    />
+                    <div className="flex flex-col gap-[5px] w-[200px] items-center">
+                        <p className="font-work-sans font-semibold text-[22px] leading-[140%]">
+                            Keepitreal
+                        </p>
+                        <p className="font-work-sans font-normal text-[16px] leading-[140%] text-[#858584]">
+                            Total Sales: 
+                            <span className="font-space-mono font-normal text-[16px] leading-[140%] text-white">
+                                {" "}34.53 ETH
+                            </span>
+                        </p>
+                    </div>
+                    <div className="absolute top-[18px] left-[20px] size-[30px] rounded-[20px] text-[#858584] bg-[#2b2b2b] 
+                    font-space-mono text-[16px] leading-[140%] grid place-items-center">
+                        1
+                    </div>
+                </div>
+
+                <div className="relative flex flex-col items-center gap-[20px] w-[240px] h-[238px] bg-[#3b3b3b] rounded-[20px] p-[20px]">
+                    <img 
+                        className="size-[120px] rounded-[100px]"
+                        src="/avatars/redheaded.png" 
+                        alt="" 
+                    />
+                    <div className="flex flex-col gap-[5px] w-[200px] items-center">
+                        <p className="font-work-sans font-semibold text-[22px] leading-[140%]">
+                            Keepitreal
+                        </p>
+                        <p className="font-work-sans font-normal text-[16px] leading-[140%] text-[#858584]">
+                            Total Sales: 
+                            <span className="font-space-mono font-normal text-[16px] leading-[140%] text-white">
+                                {" "}34.53 ETH
+                            </span>
+                        </p>
+                    </div>
+                    <div className="absolute top-[18px] left-[20px] size-[30px] rounded-[20px] text-[#858584] bg-[#2b2b2b] 
+                    font-space-mono text-[16px] leading-[140%] grid place-items-center">
+                        1
+                    </div>
+                </div>
+
+                <div className="relative flex flex-col items-center gap-[20px] w-[240px] h-[238px] bg-[#3b3b3b] rounded-[20px] p-[20px]">
+                    <img 
+                        className="size-[120px] rounded-[100px]"
+                        src="/avatars/redheaded.png" 
+                        alt="" 
+                    />
+                    <div className="flex flex-col gap-[5px] w-[200px] items-center">
+                        <p className="font-work-sans font-semibold text-[22px] leading-[140%]">
+                            Keepitreal
+                        </p>
+                        <p className="font-work-sans font-normal text-[16px] leading-[140%] text-[#858584]">
+                            Total Sales: 
+                            <span className="font-space-mono font-normal text-[16px] leading-[140%] text-white">
+                                {" "}34.53 ETH
+                            </span>
+                        </p>
+                    </div>
+                    <div className="absolute top-[18px] left-[20px] size-[30px] rounded-[20px] text-[#858584] bg-[#2b2b2b] 
+                    font-space-mono text-[16px] leading-[140%] grid place-items-center">
+                        1
+                    </div>
+                </div>
+
+                <div className="relative flex flex-col items-center gap-[20px] w-[240px] h-[238px] bg-[#3b3b3b] rounded-[20px] p-[20px]">
+                    <img 
+                        className="size-[120px] rounded-[100px]"
+                        src="/avatars/redheaded.png" 
+                        alt="" 
+                    />
+                    <div className="flex flex-col gap-[5px] w-[200px] items-center">
+                        <p className="font-work-sans font-semibold text-[22px] leading-[140%]">
+                            Keepitreal
+                        </p>
+                        <p className="font-work-sans font-normal text-[16px] leading-[140%] text-[#858584]">
+                            Total Sales: 
+                            <span className="font-space-mono font-normal text-[16px] leading-[140%] text-white">
+                                {" "}34.53 ETH
+                            </span>
+                        </p>
+                    </div>
+                    <div className="absolute top-[18px] left-[20px] size-[30px] rounded-[20px] text-[#858584] bg-[#2b2b2b] 
+                    font-space-mono text-[16px] leading-[140%] grid place-items-center">
+                        1
+                    </div>
+                </div>
+
+                <div className="relative flex flex-col items-center gap-[20px] w-[240px] h-[238px] bg-[#3b3b3b] rounded-[20px] p-[20px]">
+                    <img 
+                        className="size-[120px] rounded-[100px]"
+                        src="/avatars/redheaded.png" 
+                        alt="" 
+                    />
+                    <div className="flex flex-col gap-[5px] w-[200px] items-center">
+                        <p className="font-work-sans font-semibold text-[22px] leading-[140%]">
+                            Keepitreal
+                        </p>
+                        <p className="font-work-sans font-normal text-[16px] leading-[140%] text-[#858584]">
+                            Total Sales: 
+                            <span className="font-space-mono font-normal text-[16px] leading-[140%] text-white">
+                                {" "}34.53 ETH
+                            </span>
+                        </p>
+                    </div>
+                    <div className="absolute top-[18px] left-[20px] size-[30px] rounded-[20px] text-[#858584] bg-[#2b2b2b] 
+                    font-space-mono text-[16px] leading-[140%] grid place-items-center">
+                        1
+                    </div>
+                </div>
+
+                <div className="relative flex flex-col items-center gap-[20px] w-[240px] h-[238px] bg-[#3b3b3b] rounded-[20px] p-[20px]">
+                    <img 
+                        className="size-[120px] rounded-[100px]"
+                        src="/avatars/redheaded.png" 
+                        alt="" 
+                    />
+                    <div className="flex flex-col gap-[5px] w-[200px] items-center">
+                        <p className="font-work-sans font-semibold text-[22px] leading-[140%]">
+                            Keepitreal
+                        </p>
+                        <p className="font-work-sans font-normal text-[16px] leading-[140%] text-[#858584]">
+                            Total Sales: 
+                            <span className="font-space-mono font-normal text-[16px] leading-[140%] text-white">
+                                {" "}34.53 ETH
+                            </span>
+                        </p>
+                    </div>
+                    <div className="absolute top-[18px] left-[20px] size-[30px] rounded-[20px] text-[#858584] bg-[#2b2b2b] 
+                    font-space-mono text-[16px] leading-[140%] grid place-items-center">
+                        1
+                    </div>
+                </div>
+
+
+            </div>
+
+        </div>
+    )
+}
+
+export default TopCreatorSection
