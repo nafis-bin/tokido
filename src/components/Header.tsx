0fe6c9b9d35b6c7b3b645b055ac8ec96e8845d6c
nafis bin <nafisbinnizam@gmail.com>
2026-04-22 03:07:39 +0600

    fix: header and hero section made responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,32 +4,35 @@ import userLogo from '../assets/User.png'
 const Header = () => {
     return (
         <>
-            <header className="w-full h-[100px] px-[50px]">
+            <header className="w-full h-[100px] max-md:h-[54px] px-[50px] max-sm:px-[30px]">
                 <nav className="flex h-full bg-main items-center justify-between">
-                <div>
-                    <img 
-                    src={mainLogo} 
-                    alt="logo" 
-                    className="w-[243px] h-[32px]"
-                    />
-                </div>
-                <div className="w-[607px] h-[60px] bg-main">
-                    <ul className="w-full h-full flex items-center gap-[10px]">
-                    <li className="bg-main hover:cursor-pointer hover:text-p5 py-[12px] px-[20px] my-[7px] font-work-sans font-semibold leading-[1.4] text-white text-[16px]">
-                        Marketplace
-                    </li>
-                    <li className="bg-main hover:cursor-pointer hover:text-p5 py-[12px] px-[20px] my-[7px] font-work-sans font-semibold leading-[1.4] text-white text-[16px]">
-                        Rankings
-                    </li>
-                    <li className="bg-main hover:cursor-pointer hover:text-p5 py-[12px] px-[20px] my-[7px] font-work-sans font-semibold leading-[1.4] text-white text-[16px]">
-                        Connect a wallet
-                    </li>
-                    <button className="flex w-[152px] h-[60px] rounded-[20px] text-white px-[30px] gap-3 items-center justify-between bg-call">
-                        <img src={userLogo} alt="user logo" className="size-5 text-white" />
-                        <p className="font-work-sans font-semibold text-[16px] text-white">Sign up</p>
+                    <div>
+                        <img 
+                        src={mainLogo} 
+                        alt="logo" 
+                        className="w-[243px] h-[32px] max-md:w-[182px] max-md:h-[24px]"
+                        />
+                    </div>
+                    <div className="w-[607px] h-[60px] bg-main max-md:hidden">
+                        <ul className="w-full h-full flex items-center gap-[10px]">
+                        <li className="bg-main hover:cursor-pointer hover:text-p5 py-[12px] px-[20px] my-[7px] font-work-sans font-semibold leading-[1.4] text-white text-[16px]">
+                            Marketplace
+                        </li>
+                        <li className="bg-main hover:cursor-pointer hover:text-p5 py-[12px] px-[20px] my-[7px] font-work-sans font-semibold leading-[1.4] text-white text-[16px]">
+                            Rankings
+                        </li>
+                        <li className="bg-main hover:cursor-pointer hover:text-p5 py-[12px] px-[20px] my-[7px] font-work-sans font-semibold leading-[1.4] text-white text-[16px]">
+                            Connect a wallet
+                        </li>
+                        <button className="flex w-[152px] h-[60px] rounded-[20px] text-white px-[30px] gap-3 items-center justify-between bg-call">
+                            <img src={userLogo} alt="user logo" className="size-5 text-white" />
+                            <p className="font-work-sans font-semibold text-[16px] text-white">Sign up</p>
+                        </button>
+                        </ul>
+                    </div>
+                    <button className="hidden max-md:block w-[24px] h-[24px]">
+                        <img src="/nav-menu.png" alt="nav menu" />
                     </button>
-                    </ul>
-                </div>
                 </nav>
             </header>
         </>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,18 +3,23 @@ import heroImage from '../assets/hero-image.png'
 
 const Hero = () => {
     return (
-        <div className="max-w-[1280px] h-[704px] mx-auto bg-main">
-            <div className="bg-main w-full h-full px-[115px] py-[80px]">
-                <div className="flex gap-[30px]">
-                    <div className="flex flex-col w-[510px] h-[544px] bg-main">
-                        <p className="font-work-sans font-semibold leading-[110%] text-[67px] text-white">
+        <div className="max-w-[1280px] h-[704px] mx-auto bg-main max-md:w-full max-md:h-[541px] max-sm:h-[786px]">
+            <div className="bg-main w-full max-sm:w-[315px] h-full md:px-[115px] py-[80px] sm:px-[72px] max-sm:mx-auto max-sm:py-[40px]">
+
+                <div className="flex gap-[30px] max-sm:flex-col max-sm:gap-[40px]">
+                    <div className="flex flex-col w-[510px] h-[544px] bg-main max-md:w-[calc(100% - 374px)] max-md:h-[381px]
+                    max-sm:w-[315px] max-sm:h-[154px]">
+                        <p className="font-work-sans font-semibold leading-[110%] text-[67px] max-md:text-[38px] 
+                        max-md:leading-[120%] text-white max-sm:text-[28px] max-sm:leading-[140%]">
                             Discover Digital Art & Collect NFTs
                         </p>
-                        <p className="font-work-sans mt-[20px] leading-[160%] text-[22px] text-white">
+                        <p className="font-work-sans mt-[20px] leading-[160%] text-[22px] max-md:text-[16px] max-md:leading-[140%]
+                         text-white max-sm:mt-[10px] max-sm:text-[16px] max-sm:leading-[140%]">
                             NFT marketplace UI created with Anima for Figma. Collect, buy and sell art from
                             more than 20k NFT artists.
                         </p>
-                        <button className="flex items-center justify-center hover:cursor-pointer bg-ctoa gap-[12px] mt-[30px] w-[224px] h-[60px] rounded-[20px]">
+                        <button className="flex items-center justify-center hover:cursor-pointer bg-ctoa gap-[12px] mt-[30px] 
+                        max-md:mt-[20px] w-[224px] h-[60px] rounded-[20px] max-sm:hidden">
                             <img 
                                 className="size-[20px]"
                                 src={rocketLaunch} 
@@ -24,38 +29,49 @@ const Hero = () => {
                                 Get Started
                             </span>
                         </button>
-                        <div className="mt-[30px]">
+                        <div className="mt-[30px] max-md:mt-[20px] max-sm:hidden">
                             <div className="flex gap-[30px]">
-                                <div className="flex flex-col w-[150px] h-[77px] text-white">
-                                    <span className="font-space-mono font-bold text-[28px] leading-[140%]">
+                                <div className="flex flex-col w-[150px] h-[77px] text-white max-md:w-[90px] max-md:h-[57px]">
+                                    <span className="font-space-mono font-bold text-[28px] leading-[140%]
+                                    max-md:text-[22px] max-md:leading-[160%]">
                                         240k+
                                     </span>
-                                    <span className="font-work-sans leading-[160%] text-[24px]">
+                                    <span className="font-work-sans leading-[160%] text-[24px]
+                                    max-md:text-[16px] max-md:leading-[140%]">
                                         Total Sale
                                     </span>
                                 </div>
-                                <div className="flex flex-col w-[150px] h-[77px] text-white">
-                                    <span className="font-space-mono font-bold text-[28px] leading-[140%]">
+
+                                <div className="flex flex-col w-[150px] h-[77px] text-white max-md:w-[90px] max-md:h-[57px]">
+                                    <span className="font-space-mono font-bold text-[28px] leading-[140%]
+                                    max-md:text-[22px] max-md:leading-[160%]">
                                         100k+
                                     </span>
-                                    <span className="font-work-sans leading-[160%] text-[24px]">
+                                    <span className="font-work-sans leading-[160%] text-[24px]
+                                    max-md:text-[16px] max-md:leading-[140%]">
                                         Auctions
                                     </span>
                                 </div>
-                                <div className="flex flex-col w-[150px] h-[77px] text-white">
-                                    <span className="font-space-mono font-bold text-[28px] leading-[140%]">
+
+                                <div className="flex flex-col w-[150px] h-[77px] text-white max-md:w-[90px] max-md:h-[57px]">
+                                    <span className="font-space-mono font-bold text-[28px] leading-[140%]
+                                    max-md:text-[22px] max-md:leading-[160%]">
                                         240k+
                                     </span>
-                                    <span className="font-work-sans leading-[160%] text-[24px]">
+                                    <span className="font-work-sans leading-[160%] text-[24px]
+                                    max-md:text-[16px] max-md:leading-[140%]">
                                         Artists
                                     </span>
                                 </div>
+
                             </div>
                         </div>
                     </div>
-                    <div className="size-[510px]">
+
+                    {/* hero image  */}
+                    <div className="size-[510px] max-md:size-[330px] max-sm:size-[315px]">
                         <img 
-                            className="w-[510px] h-[401px]"
+                            className="w-[510px] h-[401px] max-md:w-[330px] max-md:h-[221px] max-sm:w-[315px] max-sm:h-[206px]"
                             src={heroImage} 
                             alt="hero image" 
                         />
@@ -69,6 +85,56 @@ const Hero = () => {
                             </div>
                         </div>
                     </div>
+
+                    <button className="hidden items-center justify-center hover:cursor-pointer bg-ctoa gap-[12px] 
+                     w-[224px] h-[60px] rounded-[20px] max-sm:flex max-sm:w-[315px] max-sm:h-[60px]">
+                        <img 
+                            className="size-[20px]"
+                            src={rocketLaunch} 
+                            alt="rocket launch logo" 
+                        />
+                        <span className="font-work-sans font-semibold text-[16px] leading-[140%] text-white">
+                            Get Started
+                        </span>
+                    </button>
+                    <div className="hidden gap-[30px] max-sm:flex">
+                            <div className="flex flex-col w-[150px] h-[77px] text-white max-md:w-[90px] max-md:h-[57px]
+                            max-sm:w-[85px] max-sm:h-[57px]">
+                                <span className="font-space-mono font-bold text-[28px] leading-[140%]
+                                    max-md:text-[22px] max-md:leading-[160%]">
+                                        240k+
+                                </span>
+                                <span className="font-work-sans leading-[160%] text-[24px]
+                                    max-md:text-[16px] max-md:leading-[140%]">
+                                        Total Sale
+                                </span>
+                            </div>
+
+                            <div className="flex flex-col w-[150px] h-[77px] text-white max-md:w-[90px] max-md:h-[57px]
+                            max-sm:w-[85px] max-sm:h-[57px]">
+                                <span className="font-space-mono font-bold text-[28px] leading-[140%]
+                                    max-md:text-[22px] max-md:leading-[160%]">
+                                        100k+
+                                </span>
+                                <span className="font-work-sans leading-[160%] text-[24px]
+                                    max-md:text-[16px] max-md:leading-[140%]">
+                                        Auctions
+                                </span>
+                            </div>
+
+                            <div className="flex flex-col w-[150px] h-[77px] text-white max-md:w-[90px] max-md:h-[57px]
+                            max-sm:w-[85px] max-sm:h-[57px]">
+                                <span className="font-space-mono font-bold text-[28px] leading-[140%]
+                                    max-md:text-[22px] max-md:leading-[160%]">
+                                        240k+
+                                </span>
+                                <span className="font-work-sans leading-[160%] text-[24px]
+                                    max-md:text-[16px] max-md:leading-[140%]">
+                                        Artists
+                                </span>
+                            </div>
+                    </div>
+
                 </div>
             </div>
         </div>
