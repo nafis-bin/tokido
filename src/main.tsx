bf336db861b75b568313384391801acc181a9c7a
nafis bin <nafisbinnizam@gmail.com>
2026-05-02 03:44:30 +0600

    fix: eslint error fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+// import App from './App.tsx'
+
+// import the generated route tree
+import { routeTree } from './routeTree.gen'
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+
+// create a new router instance
+const router = createRouter({ routeTree })
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+	interface Register {
+		router: typeof router
+	}
+}
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+	<RouterProvider router={router} />
   </StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
     tailwindcss(),
     svgr(),
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react(),
   ],
 })
