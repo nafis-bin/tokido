2104ae7599006e63c2e8f4b3e20b55068cd7f344
nafis bin <nafisbinnizam@gmail.com>
2026-05-02 03:50:37 +0600

    fix: removed unnecessary files

--- src/main.tsx
+++ src/main.tsx
@@ -17,11 +17,6 @@ declare module '@tanstack/react-router' {
 	}
 }
 
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
