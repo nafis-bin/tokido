8341092aa7a94b125c4d9b9eb93a7a59b2e55223
nafis bin <nafisbinnizam@gmail.com>
2026-05-06 19:12:45 +0600

    utility: added a parser for timer

---
+++ src/utilities/index.tsx
@@ -0,0 +1,34 @@
+export type Clock = {
+    hours: string,
+    minutes: string,
+    seconds: string,
+}
+
+export type Art = {
+    imgSrc: string,
+    artist: string,
+    artistIcon: string,
+    title: string,
+    price: number,
+    bid: number
+}
+
+export function parseDeadline(deadline: number): Clock {
+    const now = Date.now()
+    const diffMili = deadline - now
+
+    if (diffMili <= 0) throw new Error('Deadline expired!!!')
+
+    const hours = String(Math.floor(diffMili / (1000 * 60 * 60))).padStart(2, '0')
+
+    const minutes = String(
+        Math.floor((diffMili % (1000 * 60 * 60)) / (1000 * 60))
+    ).padStart(2, '0')
+
+    const seconds = String(
+        Math.floor((diffMili % (1000 * 60)) / 1000)
+    ).padStart(2, '0')
+
+    return { hours, minutes, seconds }
+}
+
